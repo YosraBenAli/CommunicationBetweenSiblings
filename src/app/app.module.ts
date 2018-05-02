47cc2a31ca2b6d05f72829c58e8933947b16ed63
YosraBenAli <31896951+YosraBenAli@users.noreply.github.com>
2018-05-03 00:00:51 +0100

    select control example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {Sibling1Component} from './sibling1/sibling1.component';
+import {FormsModule} from '@angular/forms';
+import {Sibling2Component} from './sibling2/sibling2.component';
+import { DataService } from './data.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Sibling1Component,
+    Sibling2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/sibling1/sibling1.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../data.service';
+
+@Component({
+  selector: 'app-sibling1',
+  templateUrl: './sibling1.component.html',
+  styleUrls: ['./sibling1.component.css']
+})
+export class Sibling1Component implements OnInit {
+  gender = ['male', 'female', 'other'];
+  selectedGender: string;
+
+  message: string;
+
+  constructor(private data: DataService) {
+  }
+
+  newMessage() {
+    this.data.changeMessage(this.selectedGender)
+  }
+
+  ngOnInit() {
+    this.data.currentMessage.subscribe(message => this.message = message)
+  }
+
+
+}
